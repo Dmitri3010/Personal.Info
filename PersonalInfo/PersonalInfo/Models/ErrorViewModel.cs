b3cdd7fd6865dc7e3c05e0aec9d3d058ca121da8
dmitri volski <dvolski@aras.com>
2020-06-03 22:29:21 +0300

    Init new project
    create empty asp core app

---
+++ PersonalInfo/PersonalInfo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PersonalInfo.Models
+{
+	public class ErrorViewModel
+	{
+		public string RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
