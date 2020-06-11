7e33ec152f35f67365051f3189c7990ceabedb6a
dmitri volski <dvolski@aras.com>
2020-06-12 00:28:11 +0300

    Implementing Auth controller

---
+++ PersonalInfo/PersonalInfo.Core/Tools/Validate.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace PersonalInfo.Core.Tools
+{
+	public static class Validate
+	{
+		public static bool Validator<T>(T x)
+		{
+			return x != null;
+		}
+	}
+}
---
+++ PersonalInfo/PersonalInfo/Controllers/AuthController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PersonalInfo.Core.Db;
+using PersonalInfo.Core.Models.Entity;
+using PersonalInfo.Core.Models.ViewModel;
+using PersonalInfo.Core.Tools;
+
+namespace PersonalInfo.Controllers
+{
+	public class AuthController : Controller
+	{
+		private readonly Context db;
+
+		public AuthController(Context context)
+		{
+			db = context;
+		}
+
+		[HttpGet]
+		public IActionResult Login()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public IActionResult Login(Auth user)
+		{
+			if (!Validate.Validator(user))
+			{
+				return RedirectToAction("Login");
+			}
+
+			var userFromDb = db.Users.FirstOrDefault(p => p.Email == user.Email);
+			if (userFromDb == null)
+			{
+				return RedirectToAction("Login");
+			}
+
+			return Sha256.Hash(user.Password + userFromDb.PasswordSalt) 
+					!= userFromDb.PasswordHash ? RedirectToAction("Login") 
+					: RedirectToAction("", "");
+		}
+
+		[HttpGet]
+		public IActionResult Register()
+		{
+			return View();
+		}
+	}
+}
