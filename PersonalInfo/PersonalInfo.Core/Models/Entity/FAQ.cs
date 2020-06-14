1fe8f15dcd23feba4d2f1692b9bea12023afa0d4
dmitri volski <dvolski@aras.com>
2020-06-14 15:13:14 +0300

    Add news and faqs entity

---
+++ PersonalInfo/PersonalInfo.Core/Models/Entity/FAQ.cs
@@ -0,0 +1,9 @@
+﻿namespace PersonalInfo.Core.Models.Entity
+{
+	public class FAQ : Base
+	{
+		public string Question { get; set; }
+
+		public string Answer { get; set; }
+	}
+}
---
+++ PersonalInfo/PersonalInfo.Core/Models/Entity/News.cs
@@ -0,0 +1,9 @@
+﻿namespace PersonalInfo.Core.Models.Entity
+{
+	public class News : Base
+	{
+		public string Title { get; set; }
+		public string Description { get; set; }
+		public bool IsArhive { get; set; }
+	}
+}
