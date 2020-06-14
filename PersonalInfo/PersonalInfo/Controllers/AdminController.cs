670189e564e6eeae31de0852d66a9cc7ad226ff5
dmitri volski <dvolski@aras.com>
2020-06-14 16:33:26 +0300

    add new controllers

---
+++ PersonalInfo/PersonalInfo/Controllers/AdminController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PersonalInfo.Core.Db;
+using PersonalInfo.Core.Models.Entity;
+
+namespace PersonalInfo.Controllers
+{
+	public class AdminController : Controller
+	{
+		private readonly Context _db;
+
+		public AdminController(Context context)
+		{
+			_db = context;
+		}
+
+		public IActionResult FAQList()
+		{
+			return View(_db.Faqs.ToList());
+		}
+
+		public IActionResult FAQAddOrUpdate(FAQ faq)
+		{
+			return View();
+		}
+	}
+}
