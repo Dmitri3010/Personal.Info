fff9e6b67389c098fe695f374d683e444d1df29d
dmitri volski <dvolski@aras.com>
2020-06-14 16:34:41 +0300

    Added auth filter

---
+++ PersonalInfo/PersonalInfo.Auth/AuthFilter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Runtime.InteropServices.ComTypes;
+using Microsoft.AspNetCore.Mvc.Filters;
+using PersonalInfo.Core.Db;
+using Remotion.Linq.Parsing.ExpressionVisitors.Transformation.PredefinedTransformations;
+
+namespace PersonalInfo.Auth
+{
+	public class AuthFilter : Attribute, IAuthorizationFilter
+	{
+		public void OnAuthorization(AuthorizationFilterContext context)
+		{
+			context.HttpContext.Request.Cookies.TryGetValue("AuthToken", out var auth_token);
+			var user = GetData.GetLoginedUserByToken(auth_token);
+		}
+	}
+}
