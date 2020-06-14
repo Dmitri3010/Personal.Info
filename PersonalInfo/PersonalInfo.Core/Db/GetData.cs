c9107cb92ed69ce05ab1c7f827983422d25e367b
dmitri volski <dvolski@aras.com>
2020-06-14 15:14:25 +0300

    Added tool for getting data from db

---
+++ PersonalInfo/PersonalInfo.Core/Db/GetData.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using PersonalInfo.Core.Models.Entity;
+
+namespace PersonalInfo.Core.Db
+{
+	public static class GetData
+	{
+		public static LogginedUser GetLoginedUserByToken(string token)
+		{
+			using (var context = new Context(new DbContextOptions<Context>()))
+			{
+				return context.LogginedUsers.FirstOrDefault(p => p.AuthToken.ToString() == token);
+			}
+		}
+	}
+}
