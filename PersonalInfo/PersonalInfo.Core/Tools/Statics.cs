c8fee0e53c7e60976fc016224cb8bb3d8958ce02
dmitri volski <dvolski@aras.com>
2020-06-15 21:08:50 +0300

    Added static enviroments

---
+++ PersonalInfo/PersonalInfo.Core/Tools/Statics.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace PersonalInfo.Core.Tools
+{
+	public static class Statics
+	{
+		public static IConfiguration Configuration { get; set; }
+
+		public static string WebRootPath { get; set; }
+	}
+}
