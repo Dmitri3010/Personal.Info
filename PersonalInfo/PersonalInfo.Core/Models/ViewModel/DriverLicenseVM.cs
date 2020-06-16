1fcf077957d28e4d08f8a55fe0111cc6114f2a0d
dmitri volski <dvolski@aras.com>
2020-06-16 06:53:04 +0300

    Added vm for documents

---
+++ PersonalInfo/PersonalInfo.Core/Models/ViewModel/DriverLicenseVM.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.AspNetCore.Http;
+using PersonalInfo.Core.Models.Enums;
+
+namespace PersonalInfo.Core.Models.ViewModel
+{
+	public class DriverLicenseVm
+	{
+		public string Range { get; set; }
+
+		public string Number { get; set; }
+
+		public DateTimeOffset DueDate { get; set; }
+
+		public Category Category { get; set; }
+
+		public bool IsExist { get; set; }
+
+		public IFormFile CopyOfLicense { get; set; }
+
+		public Guid UserId { get; set; }
+
+	}
+}
---
+++ PersonalInfo/PersonalInfo.Core/Models/ViewModel/EducationVM.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.AspNetCore.Http;
+using PersonalInfo.Core.Models.Enums;
+
+namespace PersonalInfo.Core.Models.ViewModel
+{
+	public class EducationVM
+	{
+		public EducationType EducationType { get; set; }
+
+		public string Qualification { get; set; }
+
+		public string Specialty { get; set; }
+
+		public int DateOfGradulation { get; set; }
+
+		public Guid UserId { get; set; }
+
+		public PositionType PositionType { get; set; }
+
+		public float WorkTime { get; set; }
+
+		public IFormFile DiplomaScan { get; set; }
+
+		public IFormFile WorkBookScan { get; set; }
+	}
+}
