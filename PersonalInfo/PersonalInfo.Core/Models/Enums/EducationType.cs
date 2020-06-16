7e44762c5089b1e7d18fcaa3df564c0dd2f04be8
dmitri volski <dvolski@aras.com>
2020-06-16 06:51:53 +0300

    Added display name for all enums

--- PersonalInfo/PersonalInfo.Core/Models/Enums/EducationType.cs
+++ PersonalInfo/PersonalInfo.Core/Models/Enums/EducationType.cs
@@ -1,9 +1,14 @@
-﻿namespace PersonalInfo.Core.Models.Enums
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PersonalInfo.Core.Models.Enums
 {
 	public enum EducationType
 	{
+		[Display(Name = "Среднее")]
 		Middle = 0,
+		[Display(Name = "Высшее")]
 		Higner = 1,
+		[Display(Name = "Среднее-специальное")]
 		MiddleSpecial = 2
 	}
 }
--- PersonalInfo/PersonalInfo.Core/Models/Enums/PositionType.cs
+++ PersonalInfo/PersonalInfo.Core/Models/Enums/PositionType.cs
@@ -1,6 +1,20 @@
-﻿namespace PersonalInfo.Core.Models.Enums
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PersonalInfo.Core.Models.Enums
 {
 	public enum PositionType
 	{
+		[Display(Name = "Маляр")]
+		painter = 0,
+		[Display(Name = "Разнорабочий")]
+		diffworker = 1,
+		[Display(Name = "Бригадир")]
+		foreman = 2,
+		[Display(Name = "Сварщик")]
+		welder = 3,
+		[Display(Name = "Монтажник")]
+		installer = 4,
+		[Display(Name = "Электрик")]
+		electrician = 5
 	}
 }
